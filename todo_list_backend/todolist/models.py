02606158544fac2aca3c4317ff0f53e01e0d0e91
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-12 15:44:52 +0100

    prog

--- todo_list_backend/todolist/models.py
+++ todo_list_backend/todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from datetime import datetime   
 
 # Create your models here.
+class TodoItem(models.Model):
+    titel = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    checked = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'({self.id}) {self.titel}'
--- todo_list_backend/todolist/views.py
+++ todo_list_backend/todolist/views.py
@@ -1,9 +1,26 @@
+
 from django.shortcuts import render
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from todolist.models import TodoItem
+from todolist.serializers import TodoItemSerializer
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class TodoItemView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, format=None):
+        todos = TodoItem.objects.all()
+        serializers = TodoItemSerializer(todos, many=True)
+        return Response(serializers.data)
+    
 class LoginView(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
